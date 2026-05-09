7e0226144d69f85a4061f85538e3373fcd53789a
bekzat <intelect23235@gmail.com>
2026-05-09 21:32:31 +0500

    fix: upgrade Go version to 1.24 in Dockerfiles and update main.go import

--- order-service/cmd/order-service/main.go
+++ order-service/cmd/order-service/main.go
@@ -124,13 +124,13 @@ func loadConfig() app.Config {
 		}
 	}
 	return app.Config{
-		HTTPPort:            getEnv("HTTP_PORT", "8080"),
-		GRPCPort:            getEnv("GRPC_PORT", "9090"),
-		DBHost:              getEnv("DB_HOST", "localhost"),
-		DBPort:              getEnv("DB_PORT", "5432"),
-		DBUser:              getEnv("DB_USER", "postgres"),
-		DBPassword:          getEnv("DB_PASSWORD", "postgres"),
-		DBName:              getEnv("DB_NAME", "orders_db"),
+		HTTPPort:               getEnv("HTTP_PORT", "8080"),
+		GRPCPort:               getEnv("GRPC_PORT", "9090"),
+		DBHost:                 getEnv("DB_HOST", "localhost"),
+		DBPort:                 getEnv("DB_PORT", "5432"),
+		DBUser:                 getEnv("DB_USER", "postgres"),
+		DBPassword:             getEnv("DB_PASSWORD", "postgres"),
+		DBName:                 getEnv("DB_NAME", "orders_db"),
 		PaymentServiceGRPCAddr: getEnv("PAYMENT_SERVICE_GRPC_ADDR", "localhost:9091"),
 		PaymentTimeoutSecs:     timeoutSecs,
 		RedisAddr:              getEnv("REDIS_ADDR", "localhost:6379"),
